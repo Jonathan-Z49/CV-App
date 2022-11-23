41ef3dbe8fb51331ceb548587c110a42963efc27
Jonathan Zarate <jonathanz49.dev@gmail.com>
2022-11-22 19:58:58 -0600

    Changed Personal keyname from firstName to first_name

--- src/utility/emptyStates.ts
+++ src/utility/emptyStates.ts
@@ -1,8 +1,8 @@
 import uniqid from 'uniqid';
 
 export const emptyPersonal: Personal = {
-  firstName: '',
-  lastName: '',
+  first_name: '',
+  last_name: '',
   address: '',
   email: '',
   phone: '',
