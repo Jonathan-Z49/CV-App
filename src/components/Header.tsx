ebeb22d3816de97aa3010356e09eaf99f9e3f022
Jonathan Zarate <jonathanz49.dev@gmail.com>
2022-11-22 19:55:23 -0600

    Added header component for app title

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header id="header">
+      <h1 className="header-title">CV BUILDER</h1>
+    </header>
+  );
+};
+
+export default Header;
