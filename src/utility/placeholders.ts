44fb3a01f2261a2c9e3c4e0d21afe89149490648
Jonathan Zarate <jonathanz49.dev@gmail.com>
2022-11-22 19:56:32 -0600

    Added placeholders file which holds a map of [key, formatted placeholder] for input text placeholders.

---
+++ src/utility/placeholders.ts
@@ -0,0 +1,21 @@
+import { emptyEducation, emptyPersonal, emptyWork } from './emptyStates';
+
+const placeholders: Map<string, string> = new Map();
+
+const emptyStates = [emptyPersonal, emptyWork[0], emptyEducation[0]];
+
+emptyStates.forEach((stateObj) => {
+  Object.entries(stateObj).forEach((entry) => {
+    placeholders.set(entry[0], formatKeyToPlaceholder(entry[0]));
+  });
+});
+
+function formatKeyToPlaceholder(word: string): string {
+  let tempWord = word;
+  if (word.includes('_')) {
+    tempWord = word.replace('_', ' ');
+  }
+  return tempWord.charAt(0).toUpperCase() + tempWord.slice(1);
+}
+
+export default placeholders;
