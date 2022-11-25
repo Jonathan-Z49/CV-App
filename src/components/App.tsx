791e8df21e0da5d16916dc7bc5eaba653fca9c0c
Jonathan Zarate <jonathanz49.dev@gmail.com>
2022-11-25 02:28:04 -0600

    Added skills state so that users can also display their skills in the CV.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,13 +3,20 @@ import '../css/App.scss';
 import React, { useState } from 'react';
 import uniqid from 'uniqid';
 
-import { emptyEducation, emptyPersonal, emptyWork } from '../utility/emptyStates';
+import {
+  emptyEducation,
+  emptyPersonal,
+  emptySkills,
+  emptyWork,
+} from '../utility/emptyStates';
 import SectionInput from './SectionInput';
+import ViewCV from './ViewCV';
 
 const App = () => {
   const [personal, setPersonal] = useState<Personal>(emptyPersonal);
   const [work, setWork] = useState<WorkExperience[]>(emptyWork);
   const [education, setEducation] = useState<Education[]>(emptyEducation);
+  const [skills, setSkills] = useState<Skills[]>(emptySkills);
 
   const handleChangePersonal = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPersonal((currentState) => ({
@@ -19,29 +26,55 @@ const App = () => {
   };
 
   const handleChangeWork = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setWork((currentState) => ({
-      ...currentState,
-      [e.target.name]: e.target.value,
-    }));
+    setWork((current) =>
+      current.map((obj) => {
+        if (e.target.getAttribute('data-uniqid') === obj.id) {
+          return { ...obj, [e.target.name]: e.target.value };
+        }
+        return obj;
+      }),
+    );
   };
 
   const handleChangeEducation = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEducation((currentState) => ({
-      ...currentState,
-      [e.target.name]: e.target.value,
-    }));
+    setEducation((current) =>
+      current.map((obj) => {
+        if (e.target.getAttribute('data-uniqid') === obj.id) {
+          return { ...obj, [e.target.name]: e.target.value };
+        }
+        return obj;
+      }),
+    );
+  };
+
+  const handleChangeSkills = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSkills((current) =>
+      current.map((obj) => {
+        if (e.target.getAttribute('data-uniqid') === obj.id) {
+          return { ...obj, [e.target.name]: e.target.value };
+        }
+        return obj;
+      }),
+    );
   };
 
   const handleDeleteWork = (e: React.MouseEvent<HTMLButtonElement>) => {
-    console.log(e.currentTarget.getAttribute('data-id'));
+    console.log(
+      work.filter((item) => item.id !== e.currentTarget.getAttribute('data-id')),
+    );
 
     setWork(work.filter((item) => item.id !== e.currentTarget.getAttribute('data-id')));
-    console.log(work);
   };
 
   const handleDeleteEducation = (e: React.MouseEvent<HTMLButtonElement>) => {
     setEducation(
       education.filter((item) => item.id !== e.currentTarget.getAttribute('data-id')),
+    );
+  };
+
+  const handleDeleteSkills = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setSkills(
+      skills.filter((item) => item.id !== e.currentTarget.getAttribute('data-id')),
     );
   };
 
@@ -53,19 +86,33 @@ const App = () => {
     setEducation(education.concat({ ...emptyEducation[0], id: uniqid() }));
   };
 
+  const handleAddSkills = () => {
+    setSkills(skills.concat({ ...emptySkills[0], id: uniqid() }));
+  };
+
   return (
     <div className="content">
       <SectionInput
         personal={personal}
         work={work}
         education={education}
+        skills={skills}
         changePersonal={handleChangePersonal}
         changeWork={handleChangeWork}
         changeEducation={handleChangeEducation}
+        changeSkills={handleChangeSkills}
         deleteWork={handleDeleteWork}
         deleteEducation={handleDeleteEducation}
+        deleteSkills={handleDeleteSkills}
         addWork={handleAddWork}
         addEducation={handleAddEducation}
+        addSkills={handleAddSkills}
+      />
+      <ViewCV
+        personalInfo={personal}
+        workInfo={work}
+        educationInfo={education}
+        skillsInfo={skills}
       />
     </div>
   );
